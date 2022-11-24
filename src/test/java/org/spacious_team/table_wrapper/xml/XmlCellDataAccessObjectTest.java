c3e34a7d9e2270fe205979c502da3ccc1b857413
Vitalii Ananev <vananiev@users.noreply.github.com>
2022-11-25 01:37:23 +0300

    add simple test

---
+++ src/test/java/org/spacious_team/table_wrapper/xml/XmlCellDataAccessObjectTest.java
@@ -0,0 +1,53 @@
+/*
+ * Table Wrapper Xml SpreadsheetML Impl
+ * Copyright (C) 2022  Spacious Team <spacious-team@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.spacious_team.table_wrapper.xml;
+
+import nl.fountain.xelem.excel.Cell;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class XmlCellDataAccessObjectTest {
+
+    @Spy
+    XmlCellDataAccessObject dao;
+
+    @Mock
+    Cell cell;
+
+    @Test
+    void getValueNull() {
+        when(cell.hasData()).thenReturn(false);
+        assertNull(dao.getValue(cell));
+    }
+
+    @Test
+    void getValue() {
+        when(cell.hasData()).thenReturn(true);
+        dao.getValue(cell);
+        verify(cell).getData();
+    }
+}
